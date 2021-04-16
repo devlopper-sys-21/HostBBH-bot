7458bd0e687b99bfa19e5bd20c4fa6b5641b7c2d
devlopper-sys-21 <56327675+devlopper-sys-21@users.noreply.github.com>
2021-04-16 12:29:35 -0400

    yessur

--- commands/kosrules.js
+++ commands/kosrules.js
@@ -6,7 +6,7 @@ module.exports = {
        embed = new Discord.MessageEmbed()
        .setTitle("KOS scrims rules")
        .setURL("")
-       .setDescription("__**Banned Weapons**__\n\n*The following weapons are not allowed in kos scrims:*\n\n♢ Explosive guns\n♢ SMG 101\n♢ Tactical SMG\n♢ Drum Gun\n♢ Minigun\n♢ Burst SMG\n♢ Machine Pistol\n♢Heavy Sniper\n♢ Semi-Auto Sniper\n♢ Snowball launcher\n♢ Ironman hands\n♢ Flamethrower\n♢ AUG, Burst Assualt Rifle (not the famas!)\n♢ Scoped Assault Rifle\n♢ Silenced Pistol.\n\n__**You need to follow theses rules no matter what:**__\n\n♢ No Ghosting, ghost peaking\n♢ No Bloom glitches\n♢ No Fusing\n♢ 1 ms crouch peeking is banned.")
+       .setDescription("__**Banned Weapons**__\n\n*The following weapons are not allowed in kos scrims:*\n\n♢ Explosive guns\n♢ SMG 101\n♢ Tactical SMG\n♢ Drum Gun\n♢ Minigun\n♢ Burst SMG\n♢ Machine Pistol\n♢Heavy Sniper\n♢ Semi-Auto Sniper\n♢ Snowball launcher\n♢ Ironman hands\n♢ Flamethrower\n♢ AUG, Burst Assualt Rifle (not the famas!)\n♢ Scoped Assault Rifle\n♢ Silenced Pistol.\n\n__**You need to follow theses rules no matter what:**__\n\n♢ No Ghosting, ghost peaking\n♢ No Bloom glitches\n♢ No Fusing\n♢ 1 ms crouch peeking is banned.\nHacks are not allowed!")
        .setImage("https://media.discordapp.net/attachments/625351405724631050/827988767662014494/kos_bbh.png?width=786&height=442")
        .setFooter(`kos scrims rules`)
        .setColor("#00bfff")
