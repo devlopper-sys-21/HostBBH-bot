6718ea47032e20d0b061d1248f0ce402dcdd1deb
devlopper-sys-21 <56327675+devlopper-sys-21@users.noreply.github.com>
2021-06-01 07:28:22 -0400

    eeee

--- commands/tournament1.js
+++ commands/tournament1.js
@@ -11,12 +11,13 @@ const fs = require('fs');
             
         if (message.member.hasPermission("BAN_MEMBERS")) {
             embed = new Discord.MessageEmbed()
-                .setTitle("3v3 Boxfight Tournament")
-                .setURL("https://forms.gle/qSovWvbQBaHTCMbo9")
-                .setDescription("**Hello and Welcome everybody to the first tournament of BBH!**\nWe are excited to announce you that our first tournament!\n\n**Details :**\n♢ It's a 3v3 boxfight tournament so everybody need to be in a trio!\n♢ It's in the Boxfights map.\n First to 5 wins will be qualified in the next match\n\n **Rules :**\n♢ Hacks of any type is not allowed\n♢ Ghost peaking is not allowed!\n♢ 1ms crouch peaking is not allowed\n♢ Fusing is not allowed\n♢ No bloom glitches!\n♢ Be respectfull.\n\n **If you don't follow one of the rules above your trio will automaticly get removed from the tournament.**\n\n To Sign up you need one person of your team fill this form : ** https://forms.gle/qSovWvbQBaHTCMbo9 **\n\n **IMPORTANT : IF AT ANY MOMENT THE HOST WE SUSPECT YOU OF CHEATING WE WILL ASK YOU FOR THE CLIP SO PREPARE RECORDING OR YOUR WHAT YOU USE FOR DOING CLIPS!**\n\n**The prize pool of this tournament will be 400 robux**\n1st place - 250 robux\n2nd place - 100 robux\n3rd place - 50 robux\n\n **note: We're gonna start to do weelky tournaments!**\n\n__**Tournament Date: 1 May, this Saturday 2pm EST**__")
-                .setImage("https://media.discordapp.net/attachments/661695177877880832/837350589536141373/tournaments_bbh_1_may.png?width=797&height=448")
-                .setFooter(`1/5/2021`)
-                .setColor("#ffcc33")
+                .setTitle("2v2 kos Tournament")
+                .setURL("https://forms.gle/JNL61bvjmu23UEGLA")
+                .setDescription("**Hello and Welcome everybody to the first tournament of Season 2!**\nWe are excited to host this tournament!\n\n**Details :**\n♢ It's a 2v2 Kos tournament so everybody need to be in a Duo!\n♢ It's in the FFA map.\n First duo to 40 kills will be qualified in the next match\n\n **Rules :**\n♢ Hacks of any type is not allowed\n♢ Ghost peaking is not allowed!\n♢ 1ms crouch peaking is not allowed\n♢ Fusing is not allowed\n♢ No bloom glitches!\n♢ Be ready at time!\n♢ Be respectfull.\n\n **If you don't follow one of the rules above your Duo will automaticly get removed from the tournament.**\n\n To Sign up you need one person of your team fill this form : ** https://forms.gle/JNL61bvjmu23UEGLA **\n\n **IMPORTANT : IF AT ANY MOMENT THE HOST WE SUSPECT YOU OF CHEATING WE WILL ASK YOU FOR THE CLIP SO PREPARE RECORDING OR YOUR WHAT YOU USE FOR DOING CLIPS!**\n\n**The prize pool of this tournament will be 2k robux!! **\n1st place get all the 2k! (1k each)\n\n__**Tournament Date: 28 May, this Friday 5pm EAST**__\n\n\n**Sign up fast we will pick the first 10 teams!**")
+                .setThumbnail("https://images-ext-1.discordapp.net/external/tKdc9-U2qHMr07soKs43R7Xak6ZQ_VV_bCt1IGxp0Z0/%3Fwidth%3D389%26height%3D345/https/media.discordapp.net/attachments/786777556476624957/845779764019200007/unknown.png?width=350&height=310")
+                .setImage("https://media.discordapp.net/attachments/833831383162945618/847204264015298600/friday.png?width=797&height=449")
+                .setFooter(``)
+                .setColor("#6efdff")
 
         message.channel.send(embed)
             }
--- index.js
+++ index.js
@@ -72,83 +72,41 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
  
-    if(command === 'commands'){
+    if(command === 'help'){
+      client.commands.get('help').execute(message, args);
+    }
+    else if(command === 'commands'){
       client.commands.get('commands').execute(message, args);
+    }
+    else if(command === 'command'){
+      client.commands.get('command').execute(message, args);
     }
     else if(command === 'moderationcmd'){
       client.commands.get('moderationcmd').execute(message, args);
     }
-   /*
-    else if(command === 'rulesgfx'){
-    client.commands.get('rulesgfx').execute(message, args);
-    }
-    */
-    else if(command === 'kick'){
-    client.commands.get('kick').execute(message, args);
-    }
-    else if(command === 'ban'){
-    client.commands.get('ban').execute(message, args);
-    }
-    else if(command === 'unban'){
-      client.commands.get('unban').execute(message, args);
-    }
-    /*
-    else if(command === 'tournament1'){
-      client.commands.get('tournament1').execute(message, args);
-    }
-    */
-    else if(command === 'help'){
-      client.commands.get('help').execute(message, args);
-    }
-    /*
-    else if(command === 'info'){
-      client.commands.get('info').execute(message, args);
-    }
-    else if(command === 'boostperks'){
-      client.commands.get('boostperks').execute(message, args);
-    }
-    else if(command === 'verifyinfo'){
-      client.commands.get('verifyinfo').execute(message, args);
-    }
-    else if(command === 'storeinfo'){
-      client.commands.get('storeinfo').execute(message, args);
-    }
-    else if(command === 'storeprices'){
-      client.commands.get('storeprices').execute(message, args);
-    }
-    else if(command === 'scrimrules'){
-      client.commands.get('scrimrules').execute(message, args);
-    }
-    else if(command === 'partnershipinfo'){
-      client.commands.get('partnershipinfo').execute(message, args);
-    }
-    else if(command === 'compinfo'){
-      client.commands.get('compinfo').execute(message, args);
-    }
-    else if(command === 'ranksystem'){
-      client.commands.get('ranksystem').execute(message, args);
-    }
-    else if(command === 'add'){
-      client.commands.get('add').execute(message, args);
-    }
-    else if(command === 'kosrules'){
-      client.commands.get('kosrules').execute(message, args);
-    }
-    else if(command === 'ffaboxrules'){
-      client.commands.get('ffaboxrules').execute(message, args);
-    }
-    else if(command === 'soloandduoscrimrules'){
-      client.commands.get('soloandduoscrimrules').execute(message, args);
-    }
-    */
-    else if(command === 'hostkos'){
-      client.commands.get('hostkos').execute(message, args);
-    }
     else if(command === 'members'){
       client.commands.get('members').execute(message, args);
     }
+    else if(command === 'membercount'){
+      client.commands.get('membercount').execute(message, args);
+    }
+    else if(command === 'kick'){
+      client.commands.get('kick').execute(message, args);
+    }
+    else if(command === 'ban'){
+      client.commands.get('ban').execute(message, args);
+    }
+    else if(command === 'hosttrio'){
+      client.commands.get('hosttrio').execute(message, args);
+    }
+    else if(command === 'hostkos'){
+      client.commands.get('hostkos').execute(message, args);
+    }
     else if(command === 'hostsolo'){
       client.commands.get('hostsolo').execute(message, args);
+    }
+    else if(command === 'hostduobox'){
+      client.commands.get('hostduobox').execute(message, args);
     }
     else if(command === 'hostduo'){
       client.commands.get('hostduo').execute(message, args);
@@ -156,126 +114,7 @@ client.on('message', message =>{
     else if(command === 'hostbox'){
       client.commands.get('hostbox').execute(message, args);
     }
-    else if(command === 'hostduobox'){
-      client.commands.get('hostduobox').execute(message, args);
-    }
-    else if(command === 'hosttrio'){
-      client.commands.get('hosttrio').execute(message, args);
-    }
-    /*
-    else if(command === 'bbhseason2'){
-      client.commands.get('bbhseason2').execute(message, args);
-    }
-    */
-    else if(command === 'requestbl'){
-      client.commands.get('requestbl').execute(message, args);
-    }
-    
 
-    /*
-
-    if (message.author.id == client.user.id)
-  return;
-
-  if (message.guild.id in stats == false) {
-      stats[message.guild.id] = {};
-  }
-
-  const guildStats = stats[message.guild.id];
-  if(message.author.id in guildStats === false ) {
-      guildStats[message.author.id] = {
-          xp: 0,
-          level: 0,
-          last_message: 0,
-          points: 0
-      };
-  }
-  const userStats = guildStats[message.author.id];
-    if (Date.now() - userStats.last_message > 60000 ) {
-        userStats.xp += random.int(15, 25);
-    userStats.last_message = Date.now();
-
-    const xpToNextLevel = 5 * Math.pow(userStats.level, 2) + 50 * userStats.level + 100;
-    if(userStats.xp >= xpToNextLevel) {
-        userStats.level++;
-        userStats.xp = userStats.xp - xpToNextLevel;
-        message.channel.send(message.author.username + " has reached level " + userStats.level);
-    }
-    const Addpoints = 
-
-    jsonfile.writeFileSync("stats.json", stats);
-
-
-    console.log(message.author.username + ' now has '  + userStats.xp);
-    console.log(xpToNextLevel + " XP needed for the next level."); 
-    
-    }
-    const parts = message.content.split(" ");
-
-    if(command === "level") {
-      embed = new Discord.MessageEmbed()
-       .setTitle("Level")
-       .setURL("")
-       .setDescription(`Theses are your stats, ${message.member}`)
-       .setFooter(message.author.username)
-       .setColor("#00ff80")
-       .addFields(
-        { name: 'Level', value: userStats.level + "", inline: true },
-        { name: 'Xp', value: userStats.xp, inline: true },
-        { name: "points", value: userStats.points, inline: true},
-      )
-
-       message.channel.send(embed)
-       }
-
-
-      //give points system
-
-
-       else if (command === 'give') {
-        if (args.length < 2) {
-          return message.reply('Please mention the user you want to give points and specify a number of points.');
-        }
-        async function GivePointsok (){
-
-          const guildStats = stats[message.guild.id];
-        
-
-          function getUserFromMention(mention) {
-            if (!mention) return;
-          
-            if (mention.startsWith('<@') && mention.endsWith('>')) {
-              mention = mention.slice(2, -1);
-          
-              if (mention.startsWith('!')) {
-                mention = mention.slice(1);
-              }
-          
-              return client.users.cache.get(mention);
-            }
-          }
-
-          const user = getUserFromMention(args[0]);
-        if (!user) {
-          return message.reply('Please use a proper mention if you want to give someone points.');
-        }
-        const userStats = guildStats[user.tag.points];
-        const numberOfPointsToGive = getUserFromMention(args[1]);
-        try {
-          
-        await userStats + numberOfPointsToGive;
-        } catch (error) {
-          return message.channel.send(`Failed to give **${user.tag}**: ${error}`);
-        }
-      
-        return message.channel.send(`Successfully gave points to**${user.tag}**`);
-      }
-      GivePointsok();
-      
-    }
-  */
-
-  
 });
 
 
